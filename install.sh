9b2c02026e0e572993499d93021ecc8c0d3abf3d
shazbits <romain.dura@gmail.com>
2017-01-06 16:10:38 -0500

    First commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# For testing
+home=/tmp
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+files=".Xresources .bashrc .tmux.conf"
+
+echo "These files will be overriden: $files, in $home"
+read -p "Press ENTER to continue, or CTRL+C to stop now"
+
+for file in $files; do
+    echo "  $home/$file"
+    ln -sf $dir/$file $home/$file
+done
