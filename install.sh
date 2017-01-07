b02f202c28a5c42887bf4b7121bed3b8f9a2d9d7
shazbits <romain.dura@gmail.com>
2017-01-06 21:56:26 -0500

    Move old dotfiles to ./old, better prompt, reload .bashrc

--- install.sh
+++ install.sh
@@ -4,12 +4,20 @@
 home=~
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dirold="$dir/old"
 files=".Xresources .bashrc .tmux.conf"
 
-echo "These files will be overriden: $files, in $home"
+echo -e "These files: $files will be:\n - moved from $home to $dirold\n - replaced with symlinks to new dotfiles"
 read -p "Press ENTER to continue, or CTRL+C to stop now"
 
+mkdir -p $dirold
+
 for file in $files; do
-    echo "  $home/$file"
+    mv $home/$file $dirold/$file
+    echo " $home/$file -> $dir/$file"
     ln -sf $dir/$file $home/$file
 done
+
+. ~/.bashrc
+
+echo -e ".bashrc reloaded\nOld files saved in $dirold"
